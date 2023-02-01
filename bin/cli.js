0853fbab93d29e31114ac2ab1ba91e59d1a2f15e
popensislam <s.islam.like@mail.ru>
2023-02-01 17:26:29 +0500

    version 1.1.1

---
+++ bin/cli.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { execSync } from 'child_process';
+
+const runCommand = command => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (error) {
+    console.error(`Failed to execute ${command}`, error);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[ 2 ];
+const gitCheckoutCommand = `git clone https://github.com/popensislam/create-react-isa ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+
+const installDeps = runCommand(installDepsCommand);
+
+if (!installDeps) process.exit(-1);
+
+console.log('Congratulations! You are ready. Follow the followint commands to start');
+console.log(`cd ${repoName} && npm start`);
